9457d2e629a3a2fbd751dbd8128a8eeeb14278de
Yasmina Baalla <728461@student.inholland.nl>
2026-02-25 06:38:12 +0100

    Basis CreateUserForm component en App.vue aangemaakt.

---
+++ frontend/src/components/Administrator/User/CreateUserForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h2>Nieuwe gebruiker aanmaken</h2>
+
+        <!-- Formulier veld -->
+        <form>
+            <!-- Voornaam -->
+            <label>Voornaam</label>
+            <input type="text" placeholder="Voer een voornaam in" required/>
+
+            <!-- Tussenvoegsel -->
+            <label>Tussenvoegsel</label>
+            <input type="text" placeholder="Voer een tussenvoegsel in" />
+
+            <!-- Achternaam -->
+            <label>Achternaam</label>
+            <input type="text" placeholder="Voer een achternaam in" required/>
+
+            <!-- Email -->
+            <label>Email</label>
+            <input type="email" placeholder="Voer een email in" required/>
+
+            <!-- Wachtwoord -->
+            <label>Wachtwoord</label>
+            <input type="password" placeholder="Voer een wachtwoord in" required/>
+
+            <!-- Gebruikersrol -->
+            <label>- Gebruikersrol -</label>
+            <select required>
+                <option value="" disabled selected >Kies een gebruikersrol</option>
+                <option value="student">Student</option>
+                <option value="administrator">Administrator</option>
+            </select>
+
+            <!-- Opslaan knop -->
+            <button type="submit">Gebruiker aanmaken</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+</script>
