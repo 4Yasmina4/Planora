0b52ed2374ba83a80fd0ce3f5a01403e888d76b8
Yasmina Baalla <728461@student.inholland.nl>
2026-02-26 23:40:18 +0100

    Bestand Database.php opgeschoond.

--- backend/app/Config/Database.php
+++ backend/app/Config/Database.php
@@ -15,5 +15,5 @@ try{
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    exit("Unable to connect to the database." . $e->getMessage());
+    exit("Unable to connect to the database.");
 }
