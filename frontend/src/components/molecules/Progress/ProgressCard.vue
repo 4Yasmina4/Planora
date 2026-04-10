b46a3f12fbf688ee798b7cd8208c2c77a5410318
Yasmina Baalla <728461@student.inholland.nl>
2026-04-10 17:38:57 +0200

    Voortgang wordt getoond van ingelogde student met voortgangsbalk, aantal taken en aantal voltooide taken.

---
+++ frontend/src/components/molecules/Progress/ProgressCard.vue
@@ -0,0 +1,58 @@
+<!-- Molecule: dit bestand bevat een herbruikbare voortgangskaart
+     Het toont de voortgang van een student met een voortgangsbalk
+-->
+
+<template>
+    <!-- Vakkaart -->
+    <div class="bg-white rounded-xl shadow-sm p-6 flex flex-col gap-3 hover:shadow-xl hover:scale-105 transition-transform duration-200">
+        <!-- Naam van het vak -->
+        <h1 class="text-xl font-semibold text-ocean-twilight">
+            {{ courseName }}
+        </h1>
+
+        <!-- Totaal aantal taken -->
+        <p class="text-lg"><span class="font-medium text-ocean-twilight">Totaal aantal taken:</span>
+            {{ totalTasks }}
+        </p>
+
+        <!-- Aantal voltooide taken -->
+        <p class="text-lg"><span class="font-medium text-ocean-twilight">Aantal voltooide taken:</span>
+            {{ completedTasks }}
+        </p>
+
+        <!-- Percentage voor de voortgangsbalk -->
+        <ProgressBar :percentage="taskCompletionPercentage" />
+    </div>
+</template>
+
+<script setup>
+    // Atoms (ProgressBar) importeren voor voortgangsbalk
+    import ProgressBar from '../../atoms/ProgressBar.vue'
+
+    // Props zijn waardes die van buitenaf aan het component meegegeven worden
+    defineProps({
+        // Naam van het vak
+        courseName: {
+            type: String, 
+            default: ''
+        },
+
+        // Totaal aantal taken
+        totalTasks: {
+            type: Number,
+            default: 0
+        },
+
+        // Aantal voltooide taken
+        completedTasks: {
+            type: Number,
+            default: 0
+        },
+
+        // Percentage voor de voortgangsbalk
+        taskCompletionPercentage: {
+            type: Number,
+            default: 0
+        }
+    })
+</script>
---
+++ frontend/src/components/organisms/Progress/ProgressList.vue
@@ -0,0 +1,69 @@
+<!-- Organism: dit bestand bevat de voortgang van een student
+     Het combineert de ProgressCard molecules
+-->
+
+<template>
+    <!-- Loading spinner -->
+    <LoadingSpinner v-if="isLoading" message="Voortgang worden geladen... een ogenblik geduld." />
+
+    <!-- Voortgangslijst -->
+    <div v-else class="space-y-6">
+        <ProgressCard 
+            v-for="progress in progresses"
+            :key="progress.course_id"
+            :courseName="progress.course_name"
+            :totalTasks="progress.total_tasks"
+            :completedTasks="progress.completed_tasks"
+            :taskCompletionPercentage="progress.task_completion_percentage"
+        />
+    </div>
+</template>
+
+<script setup>
+    // Ref importeren uit Vue
+    // Ref: om reactieve variabelen te maken
+    import { ref, onMounted } from 'vue';
+
+    // Atoms importeren
+    import LoadingSpinner from '../../atoms/LoadingSpinner.vue'
+    import ProgressBar from '../../atoms/ProgressBar.vue'
+
+    // Molecules importeren
+    import ProgressCard from '../../molecules/Progress/ProgressCard.vue'
+
+    // Aangepaste axios instantie importeren met JWT token interceptor
+    // Interceptor zorgt ervoor dat bij elk verzoek de JWT token automatisch wordt toegevoegd
+    // Wordt gebruikt voor het vesturen van HTTP verzoeken naar de backend
+    // Maakt het makkelijker om een token mee te sturen met elk verzoek in tegenstelling tot fetch()
+    import apiClient from '../../../utils/axios.js'
+
+    // Reactieve variabele voor de array met voortgangen
+    const progresses = ref([])
+
+    // Reactieve variabele om bij te houden of de voortgangen nog geladen worden
+    const isLoading = ref(true)
+
+    // Functie om alle voortgangen op te halen
+    // Async function zorgt ervoor dat de functie kan wachten op iets (zoals data) zonder de rest van de pagina te blokkeren
+    async function fetchProgresses() {
+        try{
+            // Laadstatus op true zetten, voordat de voortgangen worden opgehaald
+            isLoading.value = true;
+            // POST verzoek sturen naar de backend
+            const response = await apiClient.get('/progress')
+            progresses.value = response.data
+        } catch (error) {
+            // Foutmelding tonen als er iets mis gaat
+            console.error('Fout bij het ophalen van de voortgangen')
+        } finally {
+            // Finally wordt altijd uitgevoerd, ook al er een fout optreedt
+            isLoading.value = false
+        }
+    }
+
+    // onMounted wordt uitgevoerd zodra het component volledig geladen is in de browser
+    onMounted(() => {
+        // Voortgangen ophalen zodra de pagina geladen is
+        fetchProgresses()
+    })
+</script>
