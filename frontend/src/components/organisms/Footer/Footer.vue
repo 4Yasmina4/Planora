7fc772a4991ec003da3acf5e98353709e5b5ccd1
Yasmina Baalla <728461@student.inholland.nl>
2026-04-12 13:48:49 +0200

    Footer aangemaakt en toegepast op de homepagina.

---
+++ frontend/src/components/organisms/Footer/Footer.vue
@@ -0,0 +1,13 @@
+<!-- Organism: dit bestand bevat de footer. Het toont het copyright van Planora -->
+
+<template>
+    <footer class="bg-soft-periwinkle py-6">
+        <p class="text-center text-white font-semibold">
+            &copy; 2026 Planora
+        </p>
+    </footer>
+</template>
+
+<script setup>
+    // Geen script nodig
+</script>
